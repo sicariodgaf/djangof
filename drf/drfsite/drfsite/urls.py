75d09ce86363bf7c95e2eee14fe2f9c0816587ba
Artyom <asolkup@mail.ru>
2024-11-12 23:26:18 +0300

    9-10

--- drf/drfsite/drfsite/urls.py
+++ drf/drfsite/drfsite/urls.py
@@ -20,12 +20,10 @@ from django.urls import path, include
 from women.views import *
 from rest_framework import routers
 
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls))
-    # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
-    # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
+    path('api/v1/women/', WomenAPIList.as_view()),
+    path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
+    path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
 ]
--- drf/drfsite/women/views.py
+++ drf/drfsite/women/views.py
@@ -1,34 +1,31 @@
 from django.forms import model_to_dict
 from rest_framework import generics, viewsets, mixins
 from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
-from .models import Women
+from .models import Women, Category
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import WomenSerializer
 
 
-class WomenViewSet(mixins.CreateModelMixin,
-                   mixins.RetrieveModelMixin,
-                   mixins.UpdateModelMixin,
-                   mixins.ListModelMixin,
-                   GenericViewSet):
+class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
+    # permission_classes = (IsAuthenticatedOrReadOnly, )
 
-"""class WomenAPIList(generics.ListCreateAPIView):
+
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
+    permission_classes = (IsOwnerOrReadOnly, )
 
 
-class WomenAPIUpdate(generics.UpdateAPIView):
+class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-
-
-class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer"""
-
+    permission_classes = (IsAdminOrReadOnly, )
 
